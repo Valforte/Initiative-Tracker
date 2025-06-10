d0bc007d56f4c575a1dd7b4ec55b71adfec7c346
Gabriel Valforte <g.valforte@expensify.com>
2025-06-09 22:18:18 -0300

    Criado build

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,7 @@
 import { createApp } from 'vue'
 import './style.css'
-import PrimeVue from 'primevue/config';
 
 // Components
 import App from './App.vue'
 
-createApp(App).use(PrimeVue).mount('#app')
+createApp(App).mount('#app')
