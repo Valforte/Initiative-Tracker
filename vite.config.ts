fb4a52ee8e8dc71f1e5200886bc2978cfcb8c2d0
Gabriel Valforte <g.valforte@expensify.com>
2025-11-07 10:41:54 -0300

    fix: merge vitest.config.ts into vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,16 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(),tailwindcss()],
+  plugins: [vue(), tailwindcss()],
   base: './',
   build: {
     outDir: './docs'
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
   }
 })
